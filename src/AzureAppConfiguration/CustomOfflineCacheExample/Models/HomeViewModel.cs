57a08e4160e29f4baa94f92be38288599fd0460a
Kasun Kodagoda <kvkrusl@gmail.com>
2019-09-29 15:01:43 +0530

    Initial commit

---
+++ src/AzureAppConfiguration/CustomOfflineCacheExample/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomOfflineCacheExample.Models
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/AzureAppConfiguration/CustomOfflineCacheExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace CustomOfflineCacheExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost
+                .CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options =>
+                    {
+                        options
+                            .Connect(settings["ConnectionStrings:AppConfiguration"]);
+                    });
+
+                })
+                .UseStartup<Startup>();
+    }
+}
