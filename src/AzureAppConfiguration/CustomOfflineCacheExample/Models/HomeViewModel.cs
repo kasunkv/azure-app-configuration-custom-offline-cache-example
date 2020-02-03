5a173304e0a598c2baef895871a01be397271dbb
Kasun Kodagoda <kvkrusl@gmail.com>
2020-02-04 01:47:16 +0530

    Added the Music Store app with the Custom offline Cache support

--- src/AzureAppConfiguration/CustomOfflineCacheExample/Models/HomeViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomOfflineCacheExample.Models
-{
-    public class HomeViewModel
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
